d1d57225b6d1ded004d8d0efca2efbe326003c8c
ramaya947 <ramaya947@yahoo.com>
2024-11-12 23:30:59 -0500

    Started Send Form page

--- src/other/components/header/Header.js
+++ src/other/components/header/Header.js
@@ -2,7 +2,7 @@ import React, { Component, useState } from 'react';
 import { FaPhoneAlt } from "react-icons/fa";
 
 export default function Header({ setSelectedPage }) {
-    const [ page, setPage ] = useState('About');
+    const [page, setPage] = useState('About');
 
     return (
         <div class="w-full h-[12vh] border-b-[1px] border-slate-400 flex flex-row justify-between shadow-md">
@@ -13,7 +13,7 @@ export default function Header({ setSelectedPage }) {
                 {
                     ['About', 'Portfolio', 'Services'].map(section => {
                         return (
-                            <a onClick={() => { 
+                            <a onClick={() => {
                                 setPage(section);
                                 setSelectedPage(section);
                             }} class={`transition-all text-base pl-5 pr-5 h-8 content-center hover:cursor-pointer rounded-lg ${page === section ? 'bg-primary' : ' '}`}>{section}</a>
@@ -21,11 +21,14 @@ export default function Header({ setSelectedPage }) {
                     })
                 }
                 <a class="flex flex-row h-10 ml-4 shadow-md justify-evenly items-center border-gray-300 border-[1px] rounded-lg hover:cursor-pointer" href='tel:7868628440'>
-                    <FaPhoneAlt class="ml-2 mr-2" size={15}/>
+                    <FaPhoneAlt class="ml-2 mr-2" size={15} />
                     <span class="ml-2 mr-2 text-sm text-nowrap">(786) 862-8440</span>
                 </a>
                 <div class="h-[50%] border-l-2 border-solid border-slate-400 pl-2 pr-2 ml-4"></div>
-                <button class="transition-all h-10 pl-5 pr-5 mr-10 text-nowrap rounded-xl text-center justify-center bg-tertiary hover:bg-tertiary-hover">Send a Case</button>
+                <button onClick={() => {
+                    setPage('Send-Form');
+                    setSelectedPage('Send-Form');
+                }} class="transition-all h-10 pl-5 pr-5 mr-10 text-nowrap rounded-xl text-center justify-center bg-tertiary hover:bg-tertiary-hover">Send a Case</button>
             </div>
         </div>
     )
---
+++ src/pages/send-form/SendForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SendForm() {
+    return (
+        <div class="w-full h-[88vh] flex flex-col items-center justify-center overflow-y-scroll scrollbar-none">
+            <div class="w-[50%] min-h-[50%] rounded-lg bg-primary">
+                Poop
+            </div>
+        </div>
+    )
+}
