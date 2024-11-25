c77ddfe2c55558d7bf858699b4a1818cf532d77b
ramaya947 <ramaya947@yahoo.com>
2024-11-25 16:33:51 -0500

    Updates

--- src/pages/send-form/MobileSendForm.js
+++ src/pages/send-form/MobileSendForm.js
@@ -17,19 +17,19 @@ export default function MobileSendForm({
                     <span class="w-full text-sm">Fill out and Submit this form and we will reach out to you as soon as possible about working on your case.</span>
                 </div>
                 <div class="flex flex-[2] flex-row w-full items-center justify-evenly ">
-                    <input type="text" class="input bg-white rounded-md h-10 w-40 indent-2 focus:outline-none focus:border-0" placeholder='First name' onChange={(e) => { setFirstName(e.target.value) }} />
+                    <input type="text" class="input bg-white rounded-md h-10 w-[75%] indent-2 focus:outline-none focus:border-0" placeholder='First name' onChange={(e) => { setFirstName(e.target.value) }} />
                 </div>
                 <div class="flex flex-[2] flex-row w-full items-center justify-evenly ">
-                    <input type="text" class="input bg-white rounded-md h-10 w-40 indent-2 focus:outline-none focus:border-0" placeholder='Last name' onChange={(e) => { setlastName(e.target.value) }} />
+                    <input type="text" class="input bg-white rounded-md h-10 w-[75%] indent-2 focus:outline-none focus:border-0" placeholder='Last name' onChange={(e) => { setlastName(e.target.value) }} />
                 </div>
                 <div class="flex flex-[2] flex-row w-full items-center justify-evenly">
-                    <input type="tel" class="input bg-white rounded-md h-10 w-40 indent-2 focus:outline-none focus:border-0" placeholder='Phone' onChange={(e) => { setPhone(e.target.value) }} />
+                    <input type="tel" class="input bg-white rounded-md h-10 w-[75%] indent-2 focus:outline-none focus:border-0" placeholder='Phone' onChange={(e) => { setPhone(e.target.value) }} />
                 </div>
                 <div class="flex flex-[2] flex-row w-full items-center justify-evenly">
-                    <input type="email" class="input bg-white rounded-md h-10 w-40 indent-2 focus:outline-none focus:border-0" placeholder='Email' onChange={(e) => { setEmail(e.target.value) }} />
+                    <input type="email" class="input bg-white rounded-md h-10 w-[75%] indent-2 focus:outline-none focus:border-0" placeholder='Email' onChange={(e) => { setEmail(e.target.value) }} />
                 </div>
                 <div class="flex flex-[2] flex-row w-full items-center justify-evenly">
-                    <select class={`select bg-white rounded-md h-10 w-40 indent-2 text-base focus:outline-none focus:border-0 ${!service ? 'text-gray-400' : 'text-black'}`} onChange={(e) => { setService(e.target.value) }}>
+                    <select class={`select bg-white rounded-md h-10 w-[75%] indent-2 text-base focus:outline-none focus:border-0 ${!service ? 'text-gray-400' : 'text-black'}`} onChange={(e) => { setService(e.target.value) }}>
                         <option class="bg-white" disabled selected>Service</option>
                         <option class="bg-white hover:bg-primary text-black" >Service 1</option>
                         <option class="bg-white hover:bg-primary text-black" >Service 2</option>
